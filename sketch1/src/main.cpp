605ffd65bf4829855389193a0e5fbd260a3f1616
Jason Carter <skipthedevops@gmail.com>
2025-08-02 09:39:20 -0400

    Got VOC working

--- sketch1/src/main.cpp
+++ sketch1/src/main.cpp
@@ -3,12 +3,15 @@
 
 const int ledPin = LED_BUILTIN;
 const uint8_t TEMP_SENSOR_ADDRESS = 0x44;
+const uint8_t VOC_SENSOR_ADDRESS = 0x59;
 
+void retrieveVoc(uint8_t buffer[6]);
+bool retrieveTemperatureAndHumidity(uint8_t serialData[6]);
 void retrieveSerialNumber();
-void retrieveTemperatureAndHumidity();
-bool retrieveI2cValue(uint8_t address, byte command, byte data[6]);
+bool retrieveI2cValue(uint8_t address, uint8_t command[], uint8_t commandLength, uint8_t outputBuffer[], uint8_t outputBufferSize, uint8_t delayMs);
+bool retrieveI2cValueWithParameters(uint8_t address, uint8_t command[], uint8_t commandLength, uint8_t parameters[], uint8_t parametersLength, uint8_t outputBuffer[], uint8_t outputBufferSize, uint8_t delayMs);
 
-bool verifyChecksum(byte data[6]);
+bool verifyChecksum(byte data[6], int length);
 bool verifyChecksumPiece(byte data[2], byte checksum);
 byte computeCrc8Simple(byte bytes[2]);
 
@@ -17,28 +20,61 @@ void setup() {
   pinMode(ledPin, OUTPUT);
 
   Serial.begin(9600);
-  Wire.begin();
 }
 
 void loop() {
   // Turn the LED on
   digitalWrite(ledPin, HIGH);
+  Wire.begin();
 
   // Get the values
   retrieveSerialNumber();
-  retrieveTemperatureAndHumidity();
+  uint8_t buffer[6];
+  bool success = retrieveTemperatureAndHumidity(buffer);
+  if (success) {
+    retrieveVoc(buffer);
+  }
 
   // Turn the LED off
   Serial.println();
   digitalWrite(ledPin, LOW);
+  Wire.end();
 
   // Wait until we do it again
   delay(10000);
 }
 
-void retrieveTemperatureAndHumidity() {
-  uint8_t serialData[6];
-  bool checksumSuccess = retrieveI2cValue(TEMP_SENSOR_ADDRESS, 0xFD, serialData);
+void retrieveVoc(uint8_t tempAndHumidityBuffer[6]) {
+  uint8_t serialData[3] = {0, 0, 0};
+  uint8_t command[2] = {0x26, 0x0F};
+
+  bool checksumSuccess = retrieveI2cValueWithParameters(VOC_SENSOR_ADDRESS, command, 2, tempAndHumidityBuffer, 6, serialData, 3, 30);
+
+  // Check the checksums
+  char vocLabel[] = "VOC: ";
+  
+  // Print out the VOC
+  if (checksumSuccess) {
+    // Seperate out the value
+    uint16_t voc = (serialData[0] << 8) | serialData[1];
+
+    // Print the value
+    char output[50];
+    Serial.print(vocLabel);
+    sprintf(output, "%d", voc);
+    Serial.println(output);
+  } else {
+    Serial.print(vocLabel);
+    char output[50];
+    sprintf(output, "%02x%02x%02x ", serialData[0], serialData[1], serialData[2]);
+    Serial.print(output);
+    Serial.println("Checksum is invalid.");
+  }
+}
+
+bool retrieveTemperatureAndHumidity(uint8_t serialData[6]) {
+  uint8_t command[1] = {0xFD};
+  bool checksumSuccess = retrieveI2cValue(TEMP_SENSOR_ADDRESS, command, 1, serialData, 6, 10);
 
   // Check the checksums
   char temperatureLabel[] = "Temperature: ";
@@ -62,17 +98,22 @@ void retrieveTemperatureAndHumidity() {
     Serial.print(humidityLabel);
     sprintf(output, "%d%%", humidity);
     Serial.println(output);
+
+    return true;
   } else {
     Serial.print(temperatureLabel);
     Serial.println("Checksum is invalid.");
     Serial.print(humidityLabel);
     Serial.println("Checksum is invalid.");
+
+    return false;
   }
 }
 
 void retrieveSerialNumber() {
   uint8_t serialData[6];
-  bool checksumSuccess = retrieveI2cValue(TEMP_SENSOR_ADDRESS, 0x89, serialData);
+  uint8_t command[1] = {0x89};
+  bool checksumSuccess = retrieveI2cValue(TEMP_SENSOR_ADDRESS, command, 1, serialData, 6, 1);
 
   // Check the serial number cheksums
   Serial.print("Serial Number: ");
@@ -86,31 +127,49 @@ void retrieveSerialNumber() {
   }
 }
 
-bool retrieveI2cValue(uint8_t address, uint8_t command, uint8_t data[6]) {
+bool retrieveI2cValue(uint8_t address, uint8_t command[], uint8_t commandLength, uint8_t outputBuffer[], uint8_t outputBufferSize, uint8_t delayMs) {
+  uint8_t parameters[0];
+  return retrieveI2cValueWithParameters(address, command, commandLength, parameters, 0, outputBuffer, outputBufferSize, delayMs);
+}
+
+bool retrieveI2cValueWithParameters(uint8_t address, uint8_t command[], uint8_t commandLength, uint8_t parameters[], uint8_t parametersLength, uint8_t outputBuffer[], uint8_t outputBufferSize, uint8_t delayMs) {
   // Send the request
   Wire.beginTransmission(address); // Address of the device
-  Wire.write(command); // Command to send
+  for (uint8_t i = 0; i < commandLength; i++) {
+    Wire.write(command[i]);
+  }
+  for (uint8_t i = 0; i < parametersLength; i++) {
+    Wire.write(parameters[i]);
+  }
   Wire.endTransmission();
 
   // Listen for the response
-  delay(10);
-  Wire.requestFrom(address, 6u);
-  byte i = 0;
+  delay(delayMs);
+  Wire.requestFrom(address, outputBufferSize);
+  uint8_t i = 0;
   while (Wire.available()) {
     // Read the byte and store it
     char c = Wire.read();
-    if (i < 6) {
-      data[i] = c;
+    if (i < outputBufferSize) {
+      outputBuffer[i] = c;
     }
     i++;
   }
 
-  return i == 6 && verifyChecksum(data);
+  return i == outputBufferSize && verifyChecksum(outputBuffer, outputBufferSize);
 }
 
-bool verifyChecksum(byte data[6]) {
-  return verifyChecksumPiece(data, data[2]) &&
-         verifyChecksumPiece(data + 3, data[5]);
+bool verifyChecksum(byte data[], int length) {
+  if (length % 3 != 0) {
+    return false;
+  }
+
+  for (int i = 0; i < length; i += 3) {
+    if (!verifyChecksumPiece(data + i, data[i + 2])) {
+      return false;
+    }
+  }
+  return true;
 }
 
 bool verifyChecksumPiece(byte data[2], byte checksum) {
